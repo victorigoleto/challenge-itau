a40126d6caa08fa5568ae79c2fe2a5d31638de23
Victor Rigoleto <joao.rigoleto@zup.com.br>
2023-01-28 23:51:06 -0300

    package dto

---
+++ src/main/java/com/password/validation/dto/PasswordRequestDTO.java
@@ -0,0 +1,16 @@
+package com.password.validation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PasswordRequestDTO {
+    private String input;
+}
---
+++ src/main/java/com/password/validation/dto/PasswordResponseDTO.java
@@ -0,0 +1,14 @@
+package com.password.validation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PasswordResponseDTO {
+    private Boolean output;
+}
