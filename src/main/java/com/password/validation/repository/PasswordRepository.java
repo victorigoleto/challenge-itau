935acd2f6e9324e4aee1db02d7e78b2da1b4b3ba
Victor Rigoleto <joao.rigoleto@zup.com.br>
2023-01-29 00:00:26 -0300

    package repository

---
+++ src/main/java/com/password/validation/repository/PasswordRepository.java
@@ -0,0 +1,9 @@
+package com.password.validation.repository;
+
+import com.password.validation.entity.Password;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordRepository extends JpaRepository<Password, Long> {
+}
