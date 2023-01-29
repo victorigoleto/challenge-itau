8e23d871c1fc056d071d29a130b305dc02a9b19e
joaorigoletozup <joao.rigoleto@zup.com.br>
2023-01-28 23:46:44 -0300

    unversioned files

---
+++ src/main/java/com/password/validation/ApiValidacaoDeSenhasApplication.java
@@ -0,0 +1,13 @@
+package com.password.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiValidacaoDeSenhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiValidacaoDeSenhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/password/validation/ApiValidacaoDeSenhasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.password.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+
+@SpringBootTest
+class ApiValidacaoDeSenhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+ */
